bdb8c8f5e5fcf9d1603e7b895e920143d7b7e629
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-13 13:52:21 +0530

    feat

---
+++ modules/web-hosting-bucket/variable.tf
@@ -0,0 +1,34 @@
+variable "bucket_prefix" {
+  description = "prefix for hosting bucket"
+  type        = string
+  default     = "web-hosting"
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy the infrastructure"
+  type        = string
+  default     = "eu-east-1"
+}
+
+variable "environment" {
+  description = "environment for development"
+  type        = string
+  default     = "dev"
+}
+
+variable "index_document" {
+  description = "Name of the index document for the s3 bucket"
+  type        = string
+  default     = "index.html"
+}
+
+variable "error_document" {
+  description = "Name of the error document for the S3 website"
+  type        = string
+  default     = "error.html"
+}
+
+variable "html_source_dir" {
+  description = "Directory containing the HTML source files"
+  type        = string
+}
