0987e9498c82652781fc05ff8c6965e1a6ea108c
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-21 00:43:32 +0530

    feat: :bamboo: cl new features

--- main.tf
+++ main.tf
@@ -15,9 +15,15 @@ module "aws_s3_bucket" {
   error_html_source_dir = "./res/error.html"
 }
 
+module "aws_acm_certificate" {
+  source      = "./modules/acm_certificate"
+  domain_name = "*.zerocloud.click"
+}
 module "cloudFront" {
-  source             = "./modules/cloudfront"
-  project_name       = "Hosting-a-static-website-using-Amazon-S3"
-  bucket_id          = module.aws_s3_bucket.bucket_id
-  bucket_domain_name = module.aws_s3_bucket.bucket_regional_domain_name
+  source                = "./modules/cloudfront"
+  project_name          = "Hosting-a-static-website-using-Amazon-S3"
+  bucket_id             = module.aws_s3_bucket.bucket_id
+  bucket_domain_name    = module.aws_s3_bucket.bucket_regional_domain_name
+  alternate_domain_name = "mywebsite.zerocloud.click"
+  acm_certificate_arn   = module.aws_acm_certificate.acm_certificate_arn
 }
