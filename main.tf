8192d7a1a8de48927c89ead3e0c5bd8d9e5ecb3a
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-21 19:10:14 +0530

    fix : domain_validation_options issue

--- main.tf
+++ main.tf
@@ -30,19 +30,21 @@ module "aws_cloudFront" {
   # depended_module       = [module.aws_acm_certificate.acm_certificate_arn]
 }
 
+module "aws_acm_validation" {
+  source                  = "./modules/acm_validation"
+  acm_certificate_arn     = module.aws_acm_certificate.acm_certificate_arn
+  validation_record_fqdns = [for record in module.aws_acm_certificate.validation_record_fqdns : record.fqdn]
+}
 module "aws_route53" {
   source                            = "./modules/route_53"
   domain_name                       = var.dns_domain_name
   hosted_zone_id                    = var.dns_hosted_zone_id
   cloudfront_distribution_name      = module.aws_cloudFront.cdn_domain_name
   cloudfront_distribution_hosted_id = module.aws_cloudFront.hosted_zone_id
-  domain_validation_options         = module.aws_acm_validation.certificate_validation.domain_validation_options
+  domain_validation_options         = module.aws_acm_validation.domain_validation_options
+  #domain_validation_options         = module.aws_acm_validation.certificate_validation.domain_validation_options
   #domain_validation_options =  module.aws_acm_certificate_validation.certificate_validation.domain_validation_options
   #acm_certificate_arn               = module.aws_acm_certificate.acm_certificate_arn
 }
 
-module "aws_acm_validation" {
-  source                  = "./modules/acm_validation"
-  acm_certificate_arn     = module.aws_acm_certificate.acm_certificate_arn
-  validation_record_fqdns = [for record in module.aws_acm_certificate.validation_record_fqdns : record.fqdn]
-}
+
