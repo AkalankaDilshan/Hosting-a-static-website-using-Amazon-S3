da812c58c57596399d33a178c57e18183f5e2634
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-30 22:17:48 +0530

    feat: add alias to route 53

--- modules/route_53/main.tf
+++ modules/route_53/main.tf
@@ -78,7 +78,7 @@ resource "aws_acm_certificate_validation" "certificate_validation" {
 
 resource "aws_route53_record" "cloudfront_alias" {
   zone_id = var.hosted_zone_id
-  name    = var.domain_name
+  name    = "mywebsite.zerocloud.click"
   type    = "A"
   alias {
     name                   = var.cloudfront_distribution_name
