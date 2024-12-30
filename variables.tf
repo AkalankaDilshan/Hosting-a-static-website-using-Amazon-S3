543f5a202fed7c10fba8fc6f3e45bbc1e9c5c6fa
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-30 23:59:28 +0530

    feat: add my hosted zone details

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "dns_domain_name" {
   type        = string
   description = "The domain name to configure in Route 53"
+  default     = "zerocloud.click"
 }
 
 variable "dns_hosted_zone_id" {
   type        = string
   description = "The ID of the hosted zone"
+  default     = "Z06367323NLMO2KENW2SB"
 }
 
 variable "acm_certificate_domain_name" {
