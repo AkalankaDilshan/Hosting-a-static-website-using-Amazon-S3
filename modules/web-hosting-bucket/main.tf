8fe18c8a85fb70c04f3725b83f1ea40cc0b44f61
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-02 10:18:47 +0530

    fix : re-allow block_public_policy

--- modules/web-hosting-bucket/main.tf
+++ modules/web-hosting-bucket/main.tf
@@ -17,10 +17,10 @@ resource "aws_s3_bucket" "web_hosting_bucket" {
 resource "aws_s3_bucket_public_access_block" "web_hosting_bucket_access" {
   bucket = aws_s3_bucket.web_hosting_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 
